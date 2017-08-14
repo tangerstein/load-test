28b50a6c3f1a988ae97152a77b86d3ba2aeb6a5f
Tobias Angerstein <tma.95@freenet.com>
2017-08-14 11:11:02 +0200

    Add catalogue

--- locustfile.py
+++ locustfile.py
@@ -17,6 +17,7 @@ class WebTasks(TaskSet):
         self.client.get("/")
         self.client.get("/login", headers={"Authorization":"Basic %s" % base64string})
         self.client.get("/category.html")
+        self.client.get("/catalogue")
         self.client.get("/detail.html?id={}".format(item_id))
         self.client.delete("/cart")
         self.client.post("/cart", json={"id": item_id, "quantity": 1})
